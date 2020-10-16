55de746888258f3c1fb938979059611b8b754e03
シアン <shianmariohistory@yahoo.co.jp>
2020-10-16 18:43:44 +0900

    1回目のコミット

---
+++ script.js
@@ -0,0 +1,13 @@
+$(function(){
+  $('#mb-btn').click(function(){
+    $('#mb-wrapper').css('display','inline');
+  });
+  $('#btn-large').click(function(){
+    $('#mb-wrapper').css('display','inline');
+  });
+  $('.modal-header img').click(function(){
+    $('#mb-wrapper').css('display','none');
+  });
+
+
+});
